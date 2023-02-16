5855a9a742619472b3bb4b62ac54f499425b4766
gax <gax@disroot.org>
2023-02-16 10:55:38 -0300

    6a

--- boot.py
+++ boot.py
@@ -1,1 +1,9 @@
-# boot.py -- run on boot-up
+import network
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    print('connecting to network...')
+    wlan.connect('No es Ulan Bator', 'noesedupublica!')
+    while not wlan.isconnected():
+        pass
