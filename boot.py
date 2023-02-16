432f5386539bdf4d7161d2708c6adadb49fdb455
gax <gax@disroot.org>
2023-02-16 11:00:31 -0300

    6a

--- boot.py
+++ boot.py
@@ -3,7 +3,7 @@ import network
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
-    print('connecting to network...')
-    wlan.connect('No es Ulan Bator', 'noesedupublica!')
+    print('conectando a la red...')
+    wlan.connect('SSID', 'PASSWORD!')
     while not wlan.isconnected():
         pass
